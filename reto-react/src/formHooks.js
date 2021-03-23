9a11759d5497838dd105cd03f225152264e3cd57
Juan José Peña <jjpenad@uniandes.edu.co>
2021-03-22 23:41:28 -0500

    Joi verifications

--- reto-react/src/formHooks.js
+++ reto-react/src/formHooks.js
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
-const useRForm = () => {
+const useRForm = (schema) => {
 	const [ inputs, setInputs ] = useState({});
+	const [ errors, setErrors ] = useState('');
 
 	const handleSubmit = (preventDefault, event) => {
 		//Prevents the form from refreshing the page after submitted
 		if (preventDefault) event.preventDefault();
 
-		console.log('submit!!!');
+		const { error } = validate();
+		if (!error) {
+			console.log('Form submited!!!');
+		}
+		else {
+			console.log('Errors', error);
+			setErrors(error);
+		}
 	};
 
 	const handleInputChange = (event) => {
@@ -15,7 +23,13 @@ const useRForm = () => {
 		setInputs({ ...inputs, [event.target.name]: event.target.value });
 	};
 
-	return { handleSubmit, handleInputChange };
+	const validate = () => {
+		if (schema !== null) {
+			return schema.validate(inputs);
+		}
+	};
+
+	return { handleSubmit, handleInputChange, errors };
 };
 
 export default useRForm;
--- reto-react/src/rForm.js
+++ reto-react/src/rForm.js
@@ -4,11 +4,60 @@ import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
 
+import * as Joi from 'joi';
+
 import useRForm from './formHooks';
 
 function BuildForm(props) {
-	const req = props.req;
-	const { handleSubmit, handleInputChange } = useRForm();
+	let req = props.req;
+	let schema = null;
+	let schemaObj = {};
+
+	req.groups.forEach((item) => {
+		let dt = false;
+		if (item.verifications) {
+			Object.keys(item.verifications).forEach((key) => {
+				if ('dataType' in item.verifications) {
+					if (item.verifications[key] === 'string') {
+						schemaObj[[ item.control.name ]] = Joi.string();
+						dt = true;
+					}
+					else if (item.verifications[key] === 'number') {
+						schemaObj[[ item.control.name ]] = Joi.number();
+						dt = true;
+					}
+				}
+				if (dt) {
+					if (key === 'min') {
+						schemaObj[[ item.control.name ]] = schemaObj[[ item.control.name ]].min(
+							item.verifications[key]
+						);
+					}
+					else if (key === 'max') {
+						schemaObj[[ item.control.name ]] = schemaObj[[ item.control.name ]].max(
+							item.verifications[key]
+						);
+					}
+					else if (key === 'regex') {
+						schemaObj[[ item.control.name ]] = schemaObj[[ item.control.name ]].regex(
+							item.verifications[key]
+						);
+					}
+					else if (key === 'required') {
+						if (item.verifications[key]) {
+							schemaObj[[ item.control.name ]] = schemaObj[[ item.control.name ]].required();
+						}
+					}
+				}
+			});
+		}
+	});
+
+	if (schemaObj !== {}) {
+		schema = Joi.object(schemaObj);
+	}
+
+	const { handleSubmit, handleInputChange } = useRForm(schema);
 
 	let i = 0;
 	const groups = req.groups.map(
